4dc96452891a4713b52ec714c3c0f04cd4d99952
Konstantinos Tsakiltzidis <laerusk@gmail.com>
2017-10-05 13:58:47 +0300

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="abcmd",
-    version="0.2.4",
+    version="0.3.0",
     url="https://github.com/laerus/abcmd",
 
     author="Konstantinos Tsakiltzidis",
